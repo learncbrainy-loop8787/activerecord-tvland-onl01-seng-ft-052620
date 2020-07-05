455cdfcffd5ae1963a013a9496ab9cbf1f6c71b7
korey ko <koreykendrick1@gmail.com>
2020-07-05 14:35:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,8 +6,8 @@ class Actor < ActiveRecord::Base
     self.first_name + " " + last_name
   end
   def list_roles
-  self.characters.collect do |character, show|
-     self.characters.shows
+  self.characters.collect do |character|
+     character. name
   end
 end
 end
